810ad7e196417076f788e41ed9f1dd594c547697
zinniaa <zinnia12mazumder@gmail.com>
2022-01-30 22:16:08 +0530

    modification

--- app/src/main/java/com/example/safesound/Alarm.java
+++ app/src/main/java/com/example/safesound/Alarm.java
@@ -1,6 +1,13 @@
 package com.example.safesound;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Alarm extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
 }
--- app/src/main/java/com/example/safesound/SOS.java
+++ app/src/main/java/com/example/safesound/SOS.java
@@ -1,6 +1,8 @@
 package com.example.safesound;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,9 @@ public class SOS extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sos);
+    }
+
+    public void TriggerSOS(View view) {
+        Toast.makeText(getApplicationContext(), "Trigger SOS", Toast.LENGTH_LONG).show();
     }
 }
